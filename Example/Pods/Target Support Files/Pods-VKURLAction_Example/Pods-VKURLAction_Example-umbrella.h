1d68ac0044c0ab046bcbe4f0776699f0a3288e7c
chorsfield <chorsfield@defymedia.com>
2016-10-22 16:31:16 -0700

    remove generated files

--- Example/Pods/Target Support Files/Pods-VKURLAction_Example/Pods-VKURLAction_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_VKURLAction_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VKURLAction_ExampleVersionString[];
-
--- Example/Pods/Target Support Files/VKURLAction/VKURLAction-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VKURLAction : NSObject
-@end
-@implementation PodsDummy_VKURLAction
-@end
